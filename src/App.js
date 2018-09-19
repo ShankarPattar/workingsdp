8eb9404c5fbc78347b0dc3df5a515f875d440e2b
ShankarPattar <shnkrpattar975@gmail.com>
2018-09-19 23:43:25 +0530

    sdp poc

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import  BoilerplateComponent from 'newsdp';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+          <BoilerplateComponent/>
+         <p>
+          To get started, edit and save to reload. From ProjOne
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
